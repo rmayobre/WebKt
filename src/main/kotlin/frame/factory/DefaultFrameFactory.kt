a9ea9b36f0a17074d5638b519eb81f97de23eb64
Ryan Mayobre <rbmayobre4@gmail.com>
2020-04-16 23:03:18 -0400

    Finished factory methods for Data frames. Now working on Control frames.

--- src/main/kotlin/frame/factory/DefaultFrameFactory.kt
+++ src/main/kotlin/frame/factory/DefaultFrameFactory.kt
@@ -6,39 +6,14 @@ import frame.OpCode
 import java.io.ByteArrayOutputStream
 
 class DefaultFrameFactory(
+    /** Byte limit for each frame before it requires fragmentation. */
     private val sizeLimit: Int = 2048
 ) : FrameFactory {
-    override fun binary(data: ByteArray): Frame {
-        if (data.size > sizeLimit) {
-            val frame = Frame(
-                isFin = false,
-                rsv1 = false,
-                rsv2 = false,
-                rsv3 = false,
-                isMasked = false,
-                code = OpCode.BINARY,
-                length = sizeLimit,
-                payload = ByteArrayOutputStream()
-            )
-            val arrays = mutableListOf<ByteArray>()
-        } else {
-            val frame = Frame(
-                isFin = true,
-                rsv1 = false,
-                rsv2 = false,
-                rsv3 = false,
-                isMasked = false,
-                code = OpCode.BINARY,
-                length = sizeLimit,
-                payload = ByteArrayOutputStream()
-            )
-        }
-        TODO("Not yet implemented")
-    }
+    override fun binary(data: ByteArray): Frame =
+        data.toDataFrame(sizeLimit, OpCode.BINARY)
 
-    override fun text(data: String): Frame {
-        TODO("Not yet implemented")
-    }
+    override fun text(data: String): Frame =
+        data.toByteArray().toDataFrame(sizeLimit, OpCode.TEXT)
 
     override fun ping(data: ByteArray?): Frame {
         TODO("Not yet implemented")
@@ -52,29 +27,81 @@ class DefaultFrameFactory(
         TODO("Not yet implemented")
     }
 
-    private fun Frame.applyPayload(data: Array<ByteArray>): Frame {
-        val head = applyPayload(data[0]) // Apply payload to first frame.
-        val tail: Frame? = null
-        for (i in 1 until data.size) {
+    companion object {
 
+        private fun ClosureCode.toByteArray(): ByteArray {
+            TODO("Convert ClosureCode into the proper byte array for a closing frame.")
         }
 
+        private fun ByteArray?.toControlFrame(): Frame {
+            TODO("Implement conversion to control FINAL frame")
+        }
 
-        return head
-    }
+        private fun ByteArray.toDataFrame(limit: Int, code: OpCode): Frame {
+            if (size > limit) {
+                var start = limit
+                val head = toFragmentedFrame(limit, code)
+                var current = head
+                while (start < size) {
+                    val remaining = size - start
+                    current.next = when {
+                        remaining < limit -> toFinalFrame(start, remaining, code)
+                        else -> toContinuationFrame(start, limit)
+                    }
+                    current = current.next!!
+                    start += limit
+                }
+                return head
+            } else {
+                return toFinalFrame(limit, code)
+            }
+        }
 
-    private fun Frame.applyPayload(data: ByteArray?): Frame {
-        data.let { payload.write(it) }
-        return this
+        private fun ByteArray.toFinalFrame(length: Int, code: OpCode): Frame = Frame(
+            isFin = true,
+            rsv1 = false,
+            rsv2 = false,
+            rsv3 = false,
+            isMasked = false,
+            code = code,
+            length = length,
+            payload = ByteArrayOutputStream()
+        ).also { it.payload.write(this) }
+
+        /** Build the final frame. */
+        private fun ByteArray.toFinalFrame(start: Int, length: Int, code: OpCode): Frame = Frame(
+            isFin = true,
+            rsv1 = false,
+            rsv2 = false,
+            rsv3 = false,
+            isMasked = false,
+            code = code,
+            length = length,
+            payload = ByteArrayOutputStream()
+        ).also { it.payload.write(copyOfRange(start, start + length - 1)) }
+
+        /** Build a continuation frame. */
+        private fun ByteArray.toContinuationFrame(start: Int, length: Int): Frame = Frame(
+            isFin = false,
+            rsv1 = false,
+            rsv2 = false,
+            rsv3 = false,
+            isMasked = false,
+            code = OpCode.CONTINUATION,
+            length = length,
+            payload = ByteArrayOutputStream()
+        ).also { it.payload.write(copyOfRange(start, start + length - 1)) }
+
+        /** Build a fragmented frame. */
+        private fun ByteArray.toFragmentedFrame(length: Int, code: OpCode): Frame = Frame(
+            isFin = false,
+            rsv1 = false,
+            rsv2 = false,
+            rsv3 = false,
+            isMasked = false,
+            code = code,
+            length = length,
+            payload = ByteArrayOutputStream()
+        ).also { it.payload.write(copyOfRange(0, length - 1)) }
     }
-//
-//    private fun createDataFrame(data: ByteArray): Frame {
-//
-//    }
-//
-//    private fun createControlFrame(data: ByteArray): Frame {
-//        TODO("Not yet implemented")
-//    }
-//
-//    private fun createFrame()
 }
