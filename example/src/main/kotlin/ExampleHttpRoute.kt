b528202e3d67a4b7d70ff217a77f3a3c513c0b4d
rmayobre <rbmayobre4@gmail.com>
2020-06-09 20:35:16 -0400

    New live tests scripts (HTML)

--- example/src/main/kotlin/ExampleHttpRoute.kt
+++ example/src/main/kotlin/ExampleHttpRoute.kt
@@ -11,9 +11,36 @@ class ExampleHttpRoute(path: String = "/") : RestfulRoute(path) {
         println("GET: ${session.request}")
         session.keepAlive = false
         return Response.Builder(Status.OK)
-            .setBody("Successful GET request.")
+            .addHeader("Content-Type", "text/html")
+            .setBody("<!DOCTYPE html>" +
+                "<html>" +
+                "<head>" +
+                "<title>Page Title</title>" +
+                "</head>" +
+                "<body>" +
+                "" +
+                "<h1>This is a Heading</h1>" +
+                "<p>This is a paragraph.</p>" +
+                "" +
+                "</body>" +
+                "</html>")
+//            .setBody("<!DOCTYPE html>\n" +
+//                "<html lang=\"en\">\n" +
+//                "<head>\n" +
+//                "    <meta charset=\"UTF-8\">\n" +
+//                "    <title>Hello World!</title>\n" +
+//                "</head>\n" +
+//                "<body>\n" +
+//                "\n" +
+//                "</body>\n" +
+//                "</html>")
             .build().also {
                 println("Response: $it")
             }
+//        return Response.Builder(Status.OK)
+//            .setBody("Successful GET request.")
+//            .build().also {
+//                println("Response: $it")
+//            }
     }
 }
